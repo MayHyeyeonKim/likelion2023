fdd0fa04fc98bdc4fe709d3b87c6485d59807668
hyeyeonkim <hkim7963@gmail.com>
2023-10-30 19:51:45 -0700

    feat django new app LIONGRAM - posts

--- first-django/demos/views.py
+++ first-django/demos/views.py
@@ -5,4 +5,26 @@ from django.http import HttpResponse
 # Create your views here.
 def calculator(request):
     # return HttpResponse("Started Calculator Func! Let's goooo!~~")
-    return render(request, "calculator.html")
+    print(f"request = {request}")
+    print(f"request type = {type(request)}")
+    print(f"request.__dict__ = {request.__dict__}")
+    # 1. 데이터 확인
+    num1 = request.GET.get("num1")
+    num2 = request.GET.get("num2")
+    operators = request.GET.get("operators")
+
+    # 2. 계산
+    if operators == "+":
+        result = int(num1) + int(num2)
+    elif operators == "-":
+        result = int(num1) - int(num2)
+    elif operators == "*":
+        result = int(num1) * int(num2)
+    elif operators == "/":
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+
+    # 3. 응답
+
+    return render(request, "calculator.html", {"result": result})
